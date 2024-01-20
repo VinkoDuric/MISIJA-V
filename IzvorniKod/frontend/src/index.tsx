743b8dbded2d2d598fa63fa186d10811765312b4
IvZagar <iz52524@fer.hr>
2024-01-20 16:05:34 +0100

    Promjenjen index.tsx i dodana ruta za addadmin

--- IzvorniKod/frontend/src/index.tsx
+++ IzvorniKod/frontend/src/index.tsx
@@ -18,80 +18,169 @@ import { Word } from "./home/word";
 import { StudyMode } from "./home/studyMode";
 import { Quiz } from "./home/quiz";
 import { Account } from "./home/account";
+import { AddAdmin } from "./home/addadmin";
 
-const App = function() {
-    const { userInfo, updateUserInfo } = useUserContext();
-    useEffect(() => {
-        let refreshSession = function() {
-            fetch("/api/v1/auth/refresh").then((response) => {
-                if (response.ok) {
-                    return response.json()
-                }
-                throw Error('Unauthenticated.');
-            }).then(userInfo => {
-                console.log(userInfo);
-                updateUserInfo(userInfo)
-            })
-                .catch(err => {
-                    console.log("Handled error: " + err);
-                    redirect('/login');
-                });
-        }
+const App = function () {
+  const { userInfo, updateUserInfo } = useUserContext();
+  useEffect(() => {
+    let refreshSession = function () {
+      fetch("/api/v1/auth/refresh")
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw Error("Unauthenticated.");
+        })
+        .then((userInfo) => {
+          console.log(userInfo);
+          updateUserInfo(userInfo);
+        })
+        .catch((err) => {
+          console.log("Handled error: " + err);
+          redirect("/login");
+        });
+    };
 
-        refreshSession();
-        let intervalId = setInterval(refreshSession, 10 * 60 * 1000); 
+    refreshSession();
+    let intervalId = setInterval(refreshSession, 10 * 60 * 1000);
 
-        return () => {
-            clearInterval(intervalId);
-        }
-    }, []);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<Auth page={AuthPages.LOGIN} />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Auth page={AuthPages.LOGIN} />} />
 
-                {/* TODO: move this to be available only when logged in */}
-                <Route path="/home" element={<Home><Languages /></Home>} />
-                <Route path="/home/account" element={ <Home> <Account /> </Home> } />
-                <Route path="/home/:lang" element={<Home><Dictionaries /></Home>} />
-                <Route path="/changepass" element={<ChangePass />} />
-                <Route path="/dictionary/:lang/:dict?" element={<Home><Dictionary /></Home>} />
-                <Route path="/add/language" element={<Home><AddLanguage /></Home>} />
-                <Route path="/word/:wordId?" element={<Home><Word /></Home>} />
-                <Route path="/home/studyMode" element={ <Home> <StudyMode /> </Home> } />
-                <Route path="/home/Quiz/ABC" element={ <Home> <Quiz studyMode={"ABC"}/> </Home> } />
-                <Route path="/home/Quiz/text" element={ <Home> <Quiz studyMode={"text"}/> </Home> } />
-                <Route path="/home/Quiz/voice" element={ <Home> <Quiz studyMode={"voice"}/> </Home> } />
+        {/* TODO: move this to be available only when logged in */}
+        <Route
+          path="/home"
+          element={
+            <Home>
+              <Languages />
+            </Home>
+          }
+        />
+        <Route
+          path="/home/account"
+          element={
+            <Home>
+              {" "}
+              <Account />{" "}
+            </Home>
+          }
+        />
+        <Route
+          path="/home/addadmin"
+          element={
+            <Home>
+              <AddAdmin />
+            </Home>
+          }
+        />
+        <Route
+          path="/home/:lang"
+          element={
+            <Home>
+              <Dictionaries />
+            </Home>
+          }
+        />
+        <Route path="/changepass" element={<ChangePass />} />
+        <Route
+          path="/dictionary/:lang/:dict?"
+          element={
+            <Home>
+              <Dictionary />
+            </Home>
+          }
+        />
+        <Route
+          path="/add/language"
+          element={
+            <Home>
+              <AddLanguage />
+            </Home>
+          }
+        />
+        <Route
+          path="/word/:wordId?"
+          element={
+            <Home>
+              <Word />
+            </Home>
+          }
+        />
+        <Route
+          path="/home/studyMode"
+          element={
+            <Home>
+              {" "}
+              <StudyMode />{" "}
+            </Home>
+          }
+        />
+        <Route
+          path="/home/Quiz/ABC"
+          element={
+            <Home>
+              {" "}
+              <Quiz studyMode={"ABC"} />{" "}
+            </Home>
+          }
+        />
+        <Route
+          path="/home/Quiz/text"
+          element={
+            <Home>
+              {" "}
+              <Quiz studyMode={"text"} />{" "}
+            </Home>
+          }
+        />
+        <Route
+          path="/home/Quiz/voice"
+          element={
+            <Home>
+              {" "}
+              <Quiz studyMode={"voice"} />{" "}
+            </Home>
+          }
+        />
 
-                <Route path="/signin" element={<Auth page={AuthPages.SIGNIN} />} />
-                {
-                    userInfo !== null && Role[userInfo.role] === Role.UNVERIFIED_USER && (
-                        <Route path="/changepass" element={<ChangePass />} />
-                    )
-                }
-                {userInfo !== null && Role[userInfo.role] !== Role.UNVERIFIED_USER && (
-                    <>
-                        <Route path="/" element={<Home><Languages /></Home>} />
-                        <Route path="/user" element={<User />} />
-                    </>
-                ) || (
-                        <Route path="/" element={<Auth page={AuthPages.LOGIN} />} />
-                    )}
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/signin" element={<Auth page={AuthPages.SIGNIN} />} />
+        {userInfo !== null && Role[userInfo.role] === Role.UNVERIFIED_USER && (
+          <Route path="/changepass" element={<ChangePass />} />
+        )}
+        {(userInfo !== null && Role[userInfo.role] !== Role.UNVERIFIED_USER && (
+          <>
+            <Route
+              path="/"
+              element={
+                <Home>
+                  <Languages />
+                </Home>
+              }
+            />
+            <Route path="/user" element={<User />} />
+          </>
+        )) || <Route path="/" element={<Auth page={AuthPages.LOGIN} />} />}
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-    <React.StrictMode>
-        <UserContextProvider>
-            <App />
-        </UserContextProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
+  </React.StrictMode>
 );
 
 reportWebVitals(console.log);
